4ddfb85e28776ee641965d4e76210e62fc3f2c09
Mehmet <mehmetkusken@gmail.com>
2021-02-19 20:45:31 -0700

    commit tree a945113d233cf8080a5ce013b3e467c705a8d634

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor(:color, :size, :material, :condition)
+    def initialize(new)
+        @new = new
+        @cobble = cobble
+    end
+    def new
+        @new
+    end
+    def initialize(brand)
+        @brand = brand
+    end
+    def brand
+        @brand
+    end
+   def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+   end
+end
